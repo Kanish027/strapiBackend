69072806a5c11f0247cddbd3db2dfca928a04704
kanish mohariya <kanishmohariya1@gmail.com>
2023-07-06 01:33:36 +0530

    commit

---
+++ config/plugins.js
@@ -0,0 +1,18 @@
+module.exports = ({ env }) => ({
+    // ...
+    upload: {
+      config: {
+        provider: 'cloudinary',
+        providerOptions: {
+          cloud_name: ('dl1tzy5zm'),
+          api_key: ('815673965196152'),
+          api_secret: ('gSS-o79NCNR89EHnrKPTQKqqlzM'),
+        },
+        actionOptions: {
+          upload: {},
+          delete: {},
+        },
+      },
+    },
+    // ...
+  });
